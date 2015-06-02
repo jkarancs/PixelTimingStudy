17350584d18b4284c5cb24c75d6b6e452c0432a8
Janos <Janos.Karancsi@cern.ch>
2015-06-03 00:04:02 +0200

    Add RunII config file for RECO/FEVT data

---
+++ test/TimingStudy_RunIIRecoData_CMSSW7X_cfg.py
@@ -0,0 +1,122 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: --data -s RAW2DIGI,RECO --conditions auto:run2_data --datatier RECO --eventcontent RECO --process TimingStudy --fileout file:RECO.root --python_filename=test/TimingStudy_RunIIRecoData_CMSSW7X_cfg.py -n 100 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('TimingStudy')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/data/Commissioning2015/ZeroBias1/RECO/PromptReco-v1/000/245/194/00000/7EC1244A-3201-E511-B973-02163E011817.root'
+        ),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('--data nevts:100'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    fileName = cms.untracked.string('file:RECO.root'),
+    outputCommands = process.RECOEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
+
+# Schedule definition
+#process.schedule = cms.Schedule(process.raw2digi_step,process.reconstruction_step,process.endjob_step,process.RECOoutput_step)
+
+
+
+# Added for TimingStudy
+# Comment out line 69 (schedule)
+# Add a Refitter for tracks
+
+process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
+process.Refitter = process.TrackRefitterP5.clone()
+process.Refitter.src = 'generalTracks'
+process.Refitter.TrajectoryInEvent = True
+
+# TimingStudy
+process.TimingStudy = cms.EDAnalyzer("TimingStudy",
+    trajectoryInput = cms.string('Refitter'),
+    fileName = cms.string("Ntuple.root"),
+    extrapolateFrom = cms.int32(2),
+    extrapolateTo = cms.int32(1),
+    keepOriginalMissingHit = cms.bool(False),
+    usePixelCPE= cms.bool(True),
+    minNStripHits = cms.int32(0),
+    triggerNames=cms.vstring(
+        "HLT_ZeroBias",
+        "HLT_Physics",
+        "HLT_Random",
+        "HLT_PixelTracks_Multiplicity100",
+        "HLT_PixelTracks_Multiplicity80",
+        "HLT_PixelTracks_Multiplicity125",
+        "HLT_PixelTracks_Multiplicity110",
+        "HLT_PixelTracks_Multiplicity85",
+        "HLT_PixelTracks_Multiplicity70",
+        "HLT_PixelTracks_Multiplicity40",
+        "HLT_L1Tech_BSC_HighMultiplicity",
+        "HLT_JetE30_NoBPTX",
+        "HLT_JetE30_NoBPTX_NoHalo",
+        "HLT_JetE30_NoBPTX3BX_NoHalo",
+        "HLT_JetE50_NoBPTX3BX_NoHalo",
+        "HLT_60Jet10",
+        "HLT_70Jet10",
+        "HLT_70Jet13",
+        "HLT_L1Tech_BSC_minBias",
+        "HLT_MinBias"
+        ),
+    #dataPileupFile = cms.string("data/PileupHistogram_test.root"),
+    #mcPileupFile   = cms.string("data/PileupHistogram_test.root"),
+    #dataPileupHistoName = cms.string("pileup"),
+    #mcPileupHistoName = cms.string("mcpileup")
+    )
+process.TimingStudy_step = cms.Path(process.MeasurementTrackerEvent*process.Refitter*process.TimingStudy)
+
+process.schedule = cms.Schedule(process.TimingStudy_step)
